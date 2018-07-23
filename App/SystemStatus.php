8368daea045af60a92ed136ae2cb9e19a44ffdf1
mkartsev <mkartsev@bk.ru>
2018-07-23 15:10:06 +0300

    changes for php7

--- App/SystemStatus.php
+++ App/SystemStatus.php
@@ -94,8 +94,8 @@ class SystemStatus {
 
         foreach ($this->stats as $iface => $bytes)
         {
-            $this->network_in  += $bytes['end'][self::RECEIVED]    - $bytes['start'][self::RECEIVED];
-            $this->network_out += $bytes['end'][self::TRANSMITTED] - $bytes['start'][self::TRANSMITTED];
+            $this->network_in  += intval($bytes['end'][self::RECEIVED])    - intval($bytes['start'][self::RECEIVED]); //for php7, without intval we have E_NOTICE
+            $this->network_out += intval($bytes['end'][self::TRANSMITTED]) - ($bytes['start'][self::TRANSMITTED]); //for php7, without intval we have E_NOTICE
         }
 
         return [
